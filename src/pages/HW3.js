34f5b3f3f04df9aac9acff2bd767dd60511d83e8
AlipourIm <iman.alip2001@gmail.com>
2022-07-26 17:39:28 +0430

    fix back-end bugs

--- src/pages/HW3.js
+++ src/pages/HW3.js
@@ -6,24 +6,48 @@ import Header from './Header'
 
 export default function HW3() {
     const [answers, setAnswers] = useState([]);
+    const [expandedQueries, seExpandedQueries] = useState([]);
     const [type, setType] = useState('boolean');
     const [query, setQuery] = useState('');
     const [queryExp, setQueryExp] = useState(false);
     const [btnDisabled, setBtnDisabled] = useState(false);
+    const [cursor, setCursor] = useState('auto')
 
 
 
     async function hw3_query() {
         setBtnDisabled(true)
+        setCursor('wait')
         await fetch(`${DOMAIN}${type}/`, {
             method: "POST",
-            body: JSON.stringify({query: query, queryExp: queryExp, type: type, mode: 'no-cors'}),
+            body: JSON.stringify({ query: query, queryExp: queryExp, type: type, mode: 'no-cors' }),
         })
-        .then((response) => response.json()) 
-        .then((data) => {
-            setAnswers(data.answers)
+            .then((response) => response.json())
+            .then((data) => {
+                setAnswers(data.answers)
+                seExpandedQueries(data.expandedQueries)
+            })
+            .catch(e => console.log(e));
+        setCursor('auto')
+        setBtnDisabled(false)
+    }
+
+    async function searchQuery(query) {
+        setBtnDisabled(true)
+        setCursor('wait')
+        await fetch(`${DOMAIN}${type}/`, {
+            method: "POST",
+            body: JSON.stringify({ query: query, queryExp: false, type: type, mode: 'no-cors' }),
         })
-        .catch(e => console.log(e));
+            .then((response) => response.json())
+            .then((data) => {
+                setAnswers(data.answers)
+                seExpandedQueries(data.expandedQueries)
+                setQueryExp(false)
+                setQuery(query)
+            })
+            .catch(e => console.log(e));
+        setCursor('auto')
         setBtnDisabled(false)
     }
     // js here :)
@@ -31,7 +55,7 @@ export default function HW3() {
     return (
         // html here
 
-        <div className="body">
+        <div className="body" style={{cursor: cursor}}>
             <Header />
             <div className="box2">
                 <h1 className="head2">Search with</h1>
@@ -43,16 +67,16 @@ export default function HW3() {
                         placeholder="type something you like to search" value={query} onChange={(e) => setQuery(e.target.value)} />
                     <div>
 
-                        <label className='head2' id="query-exp-id" htmlFor="query-expantion"><input type="checkbox" id="query-expantion" name="query-expantion" value={queryExp} onChange={(e) => setQueryExp(e.target.checked)} />Query Expantion</label>
+                        <label className='head2' id="query-exp-id" htmlFor="query-expantion"><input type="checkbox" id="query-expantion" name="query-expantion" value={queryExp} onChange={(e) => setQueryExp(e.target.checked)} checked={queryExp}/>Query Expantion</label>
                     </div>
                     <div className='container3'>
                         <select id="model-selector" className="box3" name="model-name" value={type} onChange={(e) => setType(e.target.value)}>
                             <option value="boolean">Boolean</option>
-                            <option value="tf-idf">Tf-idf</option>
+                            <option value="tf_idf">Tf-idf</option>
                             <option value="transformer">Transformer</option>
-                            <option value="fast-text">FastText</option>
+                            <option value="fastText">FastText</option>
                         </select>
-                        <button disabled={btnDisabled} id="query-btn2" onClick={() => hw3_query()}  className={`${btnDisabled ? "btn2-disabled":"btn2"}`}>Search</button>
+                        <button disabled={btnDisabled} id="query-btn2" onClick={() => hw3_query()} className={`${btnDisabled ? "btn2-disabled" : "btn2"}`}>Search</button>
                     </div>
 
                 </div>
@@ -61,9 +85,19 @@ export default function HW3() {
 
                 <div className="container4">
                     <div className="pass-box2">
+                        {expandedQueries.length > 0 && <><br /><br /><h3 className="query--result">Did you mean: </h3></>}
+
+                        {
+                            <div className='exp_query_grid'>
+                            {expandedQueries.map((answer, i) => (
+                                <a onClick={() => searchQuery(answer)} key={i} className="a--sug" >{`<${answer}>`}</a>
+                            ))}
+                            </div>
+                        }
+                        {answers.length > 0 && <><br /><br /><h3 className="query--result"> Query Results:</h3><br /></>}
                         {
                             answers.map((answer, i) => (
-                                <p id="pass1-el2" key={i} className="pass-txt2">{answer}</p>
+                                <><p id="pass1-el2" key={i} className="pass-txt2">{`-----     ${answer}`}</p><br/></>
                             ))
                         }
                     </div>
